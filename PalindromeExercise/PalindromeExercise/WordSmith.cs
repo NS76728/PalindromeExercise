62df142993ddaa7b565c60272ba8ba53c5e0405d
Nick Stockton <phoenix76728@gmail.com>
2021-09-10 15:01:58 -0400

    initial commit

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+       
+        public bool IsAPalindrome(string input)
+        {
+          
+            char[] ch = input.ToCharArray();
+            Array.Reverse(ch);
+            string rev = new string(ch);
+            bool b = input.Equals(rev, StringComparison.OrdinalIgnoreCase);
+
+            return b;
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,13 +1,31 @@
+using PalindromeExercise;
 using System;
 using Xunit;
+
+
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("hannah", true)]
+        [InlineData("nothing", false)]
+        [InlineData(" ", true)]
+        [InlineData("", true)]
+
+        public void IsAPalindrome(string input, bool expected)
         {
+            //Arrange
+            WordSmith pal = new WordSmith();
+
+
+            //Act
+            bool actual = pal.IsAPalindrome(input);
+
+            //Assert
+            Assert.Equal(expected, actual);
 
         }
     }
